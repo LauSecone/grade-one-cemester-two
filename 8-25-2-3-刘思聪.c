b3b72c029c4df917b19b5a3b9efc95c4f984bc24
Lau Secone <liusicong1998@outlook.com>
2017-04-09 18:25:59 +0800

    Add files via upload

--- 8-25-2-3-刘思聪.c
+++ 8-25-2-3-刘思聪.c
@@ -30,7 +30,7 @@ int main() {
 		case 3: updateCommodityRecord(); break;
 		case 4: deleteCommodityRecord(); break;
 		case 5: outputCommodityRecord(); break;
-		case 6: fprintf(stdout, "程序运行结束，再见！"); return 0; break;
+		case 6: fprintf(stdout, "锟斤拷锟斤拷锟斤拷锟叫斤拷锟斤拷锟斤拷锟劫硷拷锟斤拷"); return 0; break;
 		default: fprintf(stdout, "ERROR:Unknow Numner\n"); break;
 		}
 		fprintf(stdout, "\n");
@@ -38,7 +38,7 @@ int main() {
 }
 
 void printChoice() {
-	fprintf(stdout, "请输入您的选择：\n1--创建一个100条空记录的文件\n2--输入商品记录\n3--更新商品记录\n4--删除商品记录\n5--输出全部商品记录\n6--退出程序\n");
+	fprintf(stdout, "锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷选锟斤拷锟斤拷\n1--锟斤拷锟斤拷一锟斤拷100锟斤拷锟秸硷拷录锟斤拷锟侥硷拷\n2--锟斤拷锟斤拷锟斤拷品锟斤拷录\n3--锟斤拷锟斤拷锟斤拷品锟斤拷录\n4--删锟斤拷锟斤拷品锟斤拷录\n5--锟斤拷锟斤拷全锟斤拷锟斤拷品锟斤拷录\n6--锟剿筹拷锟斤拷锟斤拷\n");
 }
 
 void creatBlankRecord() {
@@ -53,12 +53,13 @@ void creatBlankRecord() {
 		for (i = 0; i < 100; ++i) {
 			fwrite(&blankCommodity, sizeof(struct commodityData), 1, fp);
 		}
+		fflush(fp);
 		if (fclose(fp)) {
 			fprintf(stderr, "Can not close the file: commodity,dat\n");
 			return;
 		}
 		else {
-			fprintf(stdout, "100条空记录创建完毕\n");
+			fprintf(stdout, "100锟斤拷锟秸硷拷录锟斤拷锟斤拷锟斤拷锟斤拷\n");
 			return;
 		}
 	}
@@ -72,25 +73,26 @@ void inputCommodityRecord() {
 	}
 	else {
 		struct commodityData com;
-		fprintf(stdout, "请输入商品信息[ID为-1代表输入结束]：\n商品 ID：");
+		fprintf(stdout, "锟斤拷锟斤拷锟斤拷锟斤拷品锟斤拷息[ID为-1锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷]锟斤拷\n锟斤拷品 ID锟斤拷");
 		fscanf(stdin, "%d", &(com.ID));
 		while (com.ID != -1) {
-			fprintf(stdout, "商品名:");
+			fprintf(stdout, "锟斤拷品锟斤拷:");
 			fscanf(stdin, "%s", com.name);
-			fprintf(stdout, "数量：");
+			fprintf(stdout, "锟斤拷锟斤拷锟斤拷");
 			fscanf(stdin, "%d", &(com.num));
-			fprintf(stdout, "价格：");
+			fprintf(stdout, "锟桔革拷锟斤拷");
 			fscanf(stdin, "%f", &(com.pri));
 			fseek(fp, (com.ID - 1) * sizeof(struct commodityData), SEEK_SET);
 			fwrite(&com, sizeof(struct commodityData), 1, fp);
-			fprintf(stdout, "请输入商品信息[ID为-1代表输入结束]：\n商品 ID：");
+			fprintf(stdout, "锟斤拷锟斤拷锟斤拷锟斤拷品锟斤拷息[ID为-1锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷]锟斤拷\n锟斤拷品 ID锟斤拷");
 			fscanf(stdin, "%d", &(com.ID));
 		}
+		fflush(fp);
 		if (fclose(fp)) {
 			fprintf(stderr, "Can not close the file: commodity.dta\n");
 		}
 		else {
-			fprintf(stdout, "商品信息输入结束\n");
+			fprintf(stdout, "锟斤拷品锟斤拷息锟斤拷锟斤拷锟斤拷锟斤拷\n");
 		}
 		outputCommodityRecord(fp);
 		return;
@@ -104,7 +106,7 @@ void updateCommodityRecord() {
 		return;
 	}
 	else {
-		fprintf(stdout, "请输入待更新商品ID[ID为-1代表结束更新]:\n");
+		fprintf(stdout, "锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷品ID[ID为-1锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷]:\n");
 		int id, nu = 0;	//ID number
 		float pr = 0;	//price
 		char str[35] = { 0 };	//name
@@ -114,23 +116,23 @@ void updateCommodityRecord() {
 			fseek(fp, (id - 1) * sizeof(struct commodityData), SEEK_SET);
 			fread(&com, sizeof(struct commodityData), 1, fp);
 			if (com.ID == 0) {
-				fprintf(stdout, "对不起，记录号为%d的商品不存在，无法更新\n", id);
+				fprintf(stdout, "锟皆诧拷锟金，硷拷录锟斤拷为%d锟斤拷锟斤拷品锟斤拷锟斤拷锟节ｏ拷锟睫凤拷锟斤拷锟斤拷\n", id);
 			}
 			else {
-				fprintf(stdout, "原商品信息如下：\n记录号（商品ID）		商品名	        数量	价格\n");
+				fprintf(stdout, "原锟斤拷品锟斤拷息锟斤拷锟铰ｏ拷\n锟斤拷录锟脚ｏ拷锟斤拷品ID锟斤拷		锟斤拷品锟斤拷	        锟斤拷锟斤拷	锟桔革拷\n");
 				printSingleRecord(fp, id);
-				fprintf(stdout, "请输入更新信息[如某项不更新请直接按回车键]：\n商品名称：");
+				fprintf(stdout, "锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷息[锟斤拷某锟筋不锟斤拷锟斤拷锟斤拷直锟接帮拷锟截筹拷锟斤拷]锟斤拷\n锟斤拷品锟斤拷锟狡ｏ拷");
 				getchar();
 				gets(str);
 				if (strlen(str) != 0) {
 					strcpy(com.name, str);
 				}
-				fprintf(stdout, "数量：");
+				fprintf(stdout, "锟斤拷锟斤拷锟斤拷");
 				gets(str);
 				if (strlen(str) != 0) {
 					com.num = converseToInt(str);
 				}
-				fprintf(stdout, "价格：");
+				fprintf(stdout, "锟桔革拷锟斤拷");
 				gets(str);
 				if (strlen(str) != 0) {
 					com.pri = converseToFloat(str);
@@ -141,11 +143,12 @@ void updateCommodityRecord() {
 			}
 			fscanf(stdin, "%d", &id);
 		}
+		fflush(fp);
 		if (close(fp)) {
 			fprintf(stderr, "Can not close the file: commodity.dat\n");
 		}
 		else {
-			fprintf(stdout, "更新工作结束\n");
+			fprintf(stdout, "锟斤拷锟铰癸拷锟斤拷锟斤拷锟斤拷\n");
 		}
 		return;
 	}
@@ -160,19 +163,19 @@ void deleteCommodityRecord() {
 	else {
 		struct commodityData blankCommodity = { 0, "", 0, 0.0 };
 		int id;
-		fprintf(stdout, "请输入待删除商品ID[记录号为-1代表结束删除]:\n");
+		fprintf(stdout, "锟斤拷锟斤拷锟斤拷锟斤拷删锟斤拷锟斤拷品ID[锟斤拷录锟斤拷为-1锟斤拷锟斤拷锟斤拷锟斤拷删锟斤拷]:\n");
 		fscanf(stdin, "%d", &id);
 		while (id != -1) {
 			struct commodityData com = { 0 };
 			fseek(fp, (id - 1) * sizeof(struct commodityData), SEEK_SET);
 			fread(&com, sizeof(struct commodityData), 1, fp);
 			if (com.ID == 0) {
-				fprintf(stdout, "对不起，记录号为%d的商品不存在，无法进行删除操作\n", id);
+				fprintf(stdout, "锟皆诧拷锟金，硷拷录锟斤拷为%d锟斤拷锟斤拷品锟斤拷锟斤拷锟节ｏ拷锟睫凤拷锟斤拷锟斤拷删锟斤拷锟斤拷锟斤拷\n", id);
 			}
 			else {
-				fprintf(stdout, "商品信息如下：\n记录号（商品ID）		商品名	        数量	价格\n");
+				fprintf(stdout, "锟斤拷品锟斤拷息锟斤拷锟铰ｏ拷\n锟斤拷录锟脚ｏ拷锟斤拷品ID锟斤拷		锟斤拷品锟斤拷	        锟斤拷锟斤拷	锟桔革拷\n");
 				printSingleRecord(fp, id);
-				fprintf(stdout, "是否确实删除（确认按Y，取消按N)？\n");
+				fprintf(stdout, "锟角凤拷确实删锟斤拷锟斤拷确锟较帮拷Y锟斤拷取锟斤拷锟斤拷N)锟斤拷\n");
 				char ch;
 				getchar();
 				fscanf(stdin, "%c", &ch);
@@ -181,19 +184,20 @@ void deleteCommodityRecord() {
 					fwrite(&blankCommodity, sizeof(struct commodityData), 1, fp);
 				}
 				if (ch == 'N') {
-					fprintf(stdout, "您选择不删除记录号为%d的商品\n", id);
+					fprintf(stdout, "锟斤拷选锟斤拷锟斤拷删锟斤拷锟斤拷录锟斤拷为%d锟斤拷锟斤拷品\n", id);
 				}
 				fflush(fp);
 				outputCommodityRecord();
 			}
-			fprintf(stdout, "请输入待删除商品ID[记录号为-1代表结束删除]:\n");
+			fprintf(stdout, "锟斤拷锟斤拷锟斤拷锟斤拷删锟斤拷锟斤拷品ID[锟斤拷录锟斤拷为-1锟斤拷锟斤拷锟斤拷锟斤拷删锟斤拷]:\n");
 			fscanf(stdin, "%d", &id);
 		}
+		fflushi(fp);
 		if (fclose(fp)) {
 			fprintf(stderr, "Can not close the file: commodity.dat\n");
 		}
 		else {
-			fprintf(stdout, "删除工作结束\n");
+			fprintf(stdout, "删锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷\n");
 		}
 	}
 	return;
@@ -207,10 +211,11 @@ void outputCommodityRecord() {
 	}
 	else {
 		int i;
-		fprintf(stdout, "商品信息如下：\n记录号（商品ID）		商品名	        数量	价格\n");
+		fprintf(stdout, "锟斤拷品锟斤拷息锟斤拷锟铰ｏ拷\n锟斤拷录锟脚ｏ拷锟斤拷品ID锟斤拷		锟斤拷品锟斤拷	        锟斤拷锟斤拷	锟桔革拷\n");
 		for (i = 1; i <= 100; ++i) {
 			printSingleRecord(fp, i);
 		}
+		fclose(fp);
 		return;
 	}
 }
